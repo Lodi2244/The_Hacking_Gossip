9e69988de4d595352cde37e27387c5065d73170d
Lodi2244 <elodieletellier1@gmail.com>
2018-10-31 13:17:30 +0100

    Heroku connection problem solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get '/team', to: 'static_pages#team'
-  get '/contact', to: 'static_pages#contact'
   resources :gossips, except: (:destroy)
   root "gossips#index"
 end
